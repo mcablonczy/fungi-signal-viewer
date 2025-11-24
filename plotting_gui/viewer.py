1b087c49861a6bd729d4fe08d35fa81afe0b9287
mcablonczy <markablonczy28@gmail.com>
2025-11-24 16:57:45 +0100

    Reuse shared peak threshold config in _compute_peak_stats_vector

--- plotting_gui/viewer.py
+++ plotting_gui/viewer.py
@@ -1667,42 +1667,23 @@ class HDF5Viewer(QWidget):
         sigma = estimate_noise_sigma_mad(y_float)
 
 
-        # ---------- Build thresholds ----------
-        prom_val = None
-        height_val = None
+        # ---------- 2) Build thresholds ----------
+        cfg = PeakThresholdConfig(
+            use_relative=getattr(self, "peaks_use_relative", True),
+            k_prom=getattr(self, "peaks_k_prom", 0.0),
+            k_height=getattr(self, "peaks_k_height", 0.0),
+            prom_abs=getattr(self, "peaks_prom_abs", 0.0),
+            height_abs=getattr(self, "peaks_height_abs", 0.0),
+            min_dist_s=getattr(self, "peaks_min_dist_s", 0.0),
+            min_width_s=getattr(self, "peaks_min_width_s", 0.0),
+        )
+        
+        kwargs = build_peak_find_kwargs(
+            sigma=sigma,
+            fs=fs,
+            cfg=cfg,
+        )
 
-        use_rel = getattr(self, "peaks_use_relative", True)
-        if use_rel:
-            k_prom = getattr(self, "peaks_k_prom", 0.0)
-            k_height = getattr(self, "peaks_k_height", 0.0)
-            if k_prom > 0.0:
-                prom_val = k_prom * sigma
-            if k_height > 0.0:
-                height_val = k_height * sigma
-        else:
-            prom_abs = getattr(self, "peaks_prom_abs", 0.0)
-            height_abs = getattr(self, "peaks_height_abs", 0.0)
-            if prom_abs > 0.0:
-                prom_val = prom_abs
-            if height_abs > 0.0:
-                height_val = height_abs
-
-        kwargs = {}
-        if prom_val is not None and prom_val > 0.0:
-            kwargs["prominence"] = prom_val
-        if height_val is not None and height_val > 0.0:
-            kwargs["height"] = height_val
-
-        min_dist_s = getattr(self, "peaks_min_dist_s", 0.0)
-        min_width_s = getattr(self, "peaks_min_width_s", 0.0)
-        if min_dist_s > 0.0:
-            dist_samples = int(round(min_dist_s * fs))
-            if dist_samples > 0:
-                kwargs["distance"] = dist_samples
-        if min_width_s > 0.0:
-            width_samples = int(round(min_width_s * fs))
-            if width_samples > 0:
-                kwargs["width"] = width_samples
 
         # ---------- Positive & negative peaks ----------
         try:
